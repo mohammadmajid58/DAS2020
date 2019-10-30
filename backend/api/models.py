dd3d72b9957d5753dc9d782f197dc3d2130725a4
2283853l <2283853l@student.gla.ac.uk>
2019-10-30 15:43:38 +0000

    [#38] Create Models to store processed CSV data
    Created a models.py file, with a rudimentary model for storing an entity comprised of
    course codes, matriculation numbers and alphanumeric grades.
    
    * added models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Grade(models.Model):
+
+    course_code = models.Charfield('Course Code', max_length=30)
+    student = models.Charfield('Student', max_length=7)
+    alphanum = models.CharField('Alphanumeric Grade', max_length=2)
+    class Meta:
+        unique_together(('course_code', 'student'))
+
