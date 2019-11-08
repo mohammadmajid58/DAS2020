fc28b393fe27e764010717c985e6327239478869
Mohammad Majid <2324127m@student.gla.ac.uk>
2019-11-08 17:15:38 +0000

    #47 [F] Fix ESLint linting errors
    Running ESLint on the command line no longer returns errors and warnings
    
    * Fixed all errors and warnings

--- frontend/src/component/TestAPI.tsx
+++ frontend/src/component/TestAPI.tsx
@@ -13,7 +13,7 @@ class TestAPI extends Component<{}, State> {
   };
 
   componentDidMount() {
-    axios.get(`http://127.0.0.1:8000/users/1/?format=json`).then(response => {
+    axios.get("http://127.0.0.1:8000/users/1/?format=json").then(response => {
       const username = response.data.username;
       this.setState({
         loading: false,
