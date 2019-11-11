c7f8197172895063f2b60926a5727694d6fb8ecc
Mohammad Majid <2324127m@student.gla.ac.uk>
2019-11-11 07:36:14 +0000

    #49 [F] Remove TestAPI component
    Removed all frontend source code for the TestAPI Component
    
    * Removed component usage from App.tsx
    * Deleted the component itself

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import TestAPI from "./component/TestAPI";
 import CSVDropZone from "./component/CSVDropZone/CSVDropZone";
 import GetModuleMarkUnit from "./component/combined_elements/GetModuleMarkUnit";
 
 const App: React.FC = () => {
   return (
     <div className="d-flex-inline">
-      <div className="d-flex justify-content-center">
-        <TestAPI />
-      </div>
       <div className="d-flex justify-content-center">
         <CSVDropZone />
       </div>
