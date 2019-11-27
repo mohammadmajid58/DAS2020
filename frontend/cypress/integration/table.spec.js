2a0db200cb87339e876473014ff286aa17d91455
AMeikle99 <aidenmeikle@ymail.com>
2019-11-27 18:09:15 +0000

    #99 [F] Fix the Tests for DatabaseTable
    The removal of pagination caused the structure of data returned from the
    API to be changed. This meant tests using the old data format then
    failed.
    
    * I changed the format of the data used by the Cypress Routes to match
      the expected format
    * The tests were then altered to better respresent the new data format
      * Removed the test that multiple requests work since only one will be
        required

--- frontend/cypress/integration/table.spec.js
+++ frontend/cypress/integration/table.spec.js
@@ -1,248 +1,158 @@
 const SERVER_URL = "http://127.0.0.1:8000";
 const VISIT_URL = "http://localhost:3000";
 
-const emptyData = {
-  count: 0,
-  next: null,
-  previous: null,
-  results: []
-};
-const smallData = {
-  count: 3,
-  next: null,
-  previous: null,
-  results: [
-    {
-      courseCode: "ORGCHEM",
-      student: "1000007",
-      alphanum: "A1"
-    },
-    {
-      courseCode: "ORGCHEM",
-      student: "1000008",
-      alphanum: "A2"
-    },
-    {
-      courseCode: "ORGCHEM",
-      student: "1000009",
-      alphanum: "B1"
-    }
-  ]
-};
-const pageOne = {
-  count: 40,
-  next: "http://127.0.0.1:8000/grades/?page=2",
-  previous: null,
-  results: [
-    {
-      courseCode: "ORGCHEM",
-      student: "1000007",
-      alphanum: "A1"
-    },
-    {
-      courseCode: "ORGCHEM",
-      student: "1000008",
-      alphanum: "A2"
-    },
-    {
-      courseCode: "ORGCHEM",
-      student: "1000009",
-      alphanum: "B1"
-    },
-    {
-      courseCode: "ORGCHEM",
-      student: "1000010",
-      alphanum: "C3"
-    },
-    {
-      courseCode: "ORGCHEM",
-      student: "1000011",
-      alphanum: "C1"
-    },
-    {
-      courseCode: "ORGCHEM",
-      student: "1000012",
-      alphanum: "D2"
-    },
-    {
-      courseCode: "ORGCHEM",
-      student: "1000013",
-      alphanum: "A1"
-    },
-    {
-      courseCode: "ORGCHEM",
-      student: "1000014",
-      alphanum: "D1"
-    },
-    {
-      courseCode: "ORGCHEM",
-      student: "1000016",
-      alphanum: "E1"
-    },
-    {
-      courseCode: "ORGCHEM",
-      student: "1000017",
-      alphanum: "C1"
-    },
-    {
-      courseCode: "ORGCHEM",
-      student: "1000018",
-      alphanum: "C2"
-    },
-    {
-      courseCode: "ORGCHEM",
-      student: "1000019",
-      alphanum: "D2"
-    },
-    {
-      courseCode: "ORGCHEM",
-      student: "1000020",
-      alphanum: "B1"
-    },
-    {
-      courseCode: "ORGCHEM",
-      student: "1000021",
-      alphanum: "C3"
-    },
-    {
-      courseCode: "ORGCHEM",
-      student: "1000022",
-      alphanum: "C1"
-    },
-    {
-      courseCode: "ORGCHEM",
-      student: "1000023",
-      alphanum: "A1"
-    },
-    {
-      courseCode: "ORGCHEM",
-      student: "1000024",
-      alphanum: "B3"
-    },
-    {
-      courseCode: "ORGCHEM",
-      student: "1000025",
-      alphanum: "D3"
-    },
-    {
-      courseCode: "ORGCHEM",
-      student: "1000026",
-      alphanum: "C3"
-    },
-    {
-      courseCode: "ORGCHEM",
-      student: "1000027",
-      alphanum: "D2"
-    }
-  ]
-};
-const pageTwo = {
-  count: 40,
-  next: null,
-  previous: "http://127.0.0.1:8000/grades/",
-  results: [
-    {
-      courseCode: "L45PROJ",
-      student: "1000001",
-      alphanum: "A4"
-    },
-    {
-      courseCode: "L45PROJ",
-      student: "1000002",
-      alphanum: "A5"
-    },
-    {
-      courseCode: "L45PROJ",
-      student: "1000004",
-      alphanum: "A4"
-    },
-    {
-      courseCode: "L45PROJ",
-      student: "1000009",
-      alphanum: "B1"
-    },
-    {
-      courseCode: "L45PROJ",
-      student: "1000010",
-      alphanum: "B1"
-    },
-    {
-      courseCode: "L45PROJ",
-      student: "1000011",
-      alphanum: "C2"
-    },
-    {
-      courseCode: "L45PROJ",
-      student: "1000012",
-      alphanum: "B3"
-    },
-    {
-      courseCode: "L45PROJ",
-      student: "1000013",
-      alphanum: "A5"
-    },
-    {
-      courseCode: "L45PROJ",
-      student: "1000014",
-      alphanum: "B3"
-    },
-    {
-      courseCode: "L45PROJ",
-      student: "1000015",
-      alphanum: "B2"
-    },
-    {
-      courseCode: "L45PROJ",
-      student: "1000016",
-      alphanum: "B1"
-    },
-    {
-      courseCode: "L45PROJ",
-      student: "1000017",
-      alphanum: "B1"
-    },
-    {
-      courseCode: "L45PROJ",
-      student: "1000018",
-      alphanum: "A5"
-    },
-    {
-      courseCode: "L45PROJ",
-      student: "1000019",
-      alphanum: "B1"
-    },
-    {
-      courseCode: "L45PROJ",
-      student: "1000022",
-      alphanum: "C2"
-    },
-    {
-      courseCode: "L45PROJ",
-      student: "1000023",
-      alphanum: "C1"
-    },
-    {
-      courseCode: "L45PROJ",
-      student: "1000024",
-      alphanum: "B1"
-    },
-    {
-      courseCode: "L45PROJ",
-      student: "1000025",
-      alphanum: "A3"
-    },
-    {
-      courseCode: "L45PROJ",
-      student: "1000028",
-      alphanum: "B2"
-    },
-    {
-      courseCode: "L45PROJ",
-      student: "1000029",
-      alphanum: "A5"
-    }
-  ]
-};
+const emptyData = [];
+const smallData = [
+  {
+    courseCode: "ORGCHEM",
+    student: "1000007",
+    alphanum: "A1"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000008",
+    alphanum: "A2"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000009",
+    alphanum: "B1"
+  }
+];
+const twoDegreeData = [
+  {
+    courseCode: "ORGCHEM",
+    student: "1000007",
+    alphanum: "A1"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000008",
+    alphanum: "A2"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000009",
+    alphanum: "B1"
+  },
+  {
+    courseCode: "INORG",
+    student: "1000010",
+    alphanum: "A1"
+  },
+  {
+    courseCode: "INORG",
+    student: "1000011",
+    alphanum: "A2"
+  },
+  {
+    courseCode: "INORG",
+    student: "1000012",
+    alphanum: "B1"
+  }
+];
+const largeData = [
+  {
+    courseCode: "ORGCHEM",
+    student: "1000007",
+    alphanum: "A1"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000008",
+    alphanum: "A2"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000009",
+    alphanum: "B1"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000010",
+    alphanum: "C3"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000011",
+    alphanum: "C1"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000012",
+    alphanum: "D2"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000013",
+    alphanum: "A1"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000014",
+    alphanum: "D1"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000016",
+    alphanum: "E1"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000017",
+    alphanum: "C1"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000018",
+    alphanum: "C2"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000019",
+    alphanum: "D2"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000020",
+    alphanum: "B1"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000021",
+    alphanum: "C3"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000022",
+    alphanum: "C1"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000023",
+    alphanum: "A1"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000024",
+    alphanum: "B3"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000025",
+    alphanum: "D3"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000026",
+    alphanum: "C3"
+  },
+  {
+    courseCode: "ORGCHEM",
+    student: "1000027",
+    alphanum: "D2"
+  }
+];
 
 describe("Database Table Loads", () => {
   before(() => {
@@ -276,7 +186,6 @@ describe("Database Table Renders Data", () => {
       method: "GET",
       status: 200
     }).as("getModuleData");
-
     cy.get(".getModuleDataButton").click();
     cy.wait(["@getModuleData"]);
 
@@ -288,37 +197,17 @@ describe("Database Table Renders Data", () => {
       .should("contain", "No records to display");
   });
 
-  it("Handles One Request Worth of Data", () => {
+  it("Handles A Data Request", () => {
     cy.route({
       url: `${SERVER_URL}/grades/`,
       response: smallData,
       method: "GET",
       status: 200
     }).as("getModuleData");
-
     cy.get(".getModuleDataButton").click();
     cy.wait(["@getModuleData"]);
+
     cy.get("span.MuiTypography-root").should("contain", "1-3 of 3");
-  });
-
-  it("Handles Multiple Requests Worth of Data", () => {
-    cy.route({
-      url: `${SERVER_URL}/grades/`,
-      response: pageOne,
-      method: "GET",
-      status: 200
-    }).as("getModuleData");
-    cy.route({
-      url: `${SERVER_URL}/grades/?page=2`,
-      response: pageTwo,
-      method: "GET",
-      status: 200
-    }).as("getModuleDataPage2");
-
-    cy.get(".getModuleDataButton").click();
-    cy.wait("@getModuleData");
-    cy.wait("@getModuleDataPage2");
-    cy.get("span.MuiTypography-root").should("contain", "1-10 of 40");
   });
 });
 
@@ -328,43 +217,28 @@ describe("Database Table Pagination Works", () => {
     cy.server();
     cy.route({
       url: `${SERVER_URL}/grades/`,
-      response: pageOne,
+      response: largeData,
       method: "GET",
       status: 200
     }).as("getModuleData");
-
-    cy.route({
-      url: `${SERVER_URL}/grades/?page=2`,
-      response: pageTwo,
-      method: "GET",
-      status: 200
-    }).as("getModuleDataPage2");
     cy.get(".getModuleDataButton").click();
-    cy.wait("@getModuleData")
-      .its("status")
-      .should("eq", 200);
-    cy.wait("@getModuleDataPage2")
-      .its("status")
-      .should("eq", 200);
-    cy.wait("@getModuleDataPage2")
-      .its("status")
-      .should("eq", 200);
+    cy.wait(["@getModuleData"]);
   });
 
   it("Can Move to Next/Previous Page", () => {
     cy.get("#root").find("span[title='Next Page']");
     cy.get("span[title='Next Page']").click();
-    cy.get(".MuiTypography-root").should("contain", "11-20 of 40");
+    cy.get(".MuiTypography-root").should("contain", "11-20 of 20");
     cy.get("span[title='Previous Page']").click();
-    cy.get(".MuiTypography-root").should("contain", "1-10 of 40");
+    cy.get(".MuiTypography-root").should("contain", "1-10 of 20");
   });
 
   it("Can Move to First/Last Page", () => {
     cy.get("#root").find("span[title='Next Page']");
     cy.get("span[title='Last Page']").click();
-    cy.get("span.MuiTypography-root").should("contain", "31-40 of 40");
+    cy.get("span.MuiTypography-root").should("contain", "11-20 of 20");
     cy.get("span[title='First Page']").click();
-    cy.get("span.MuiTypography-root").should("contain", "1-10 of 40");
+    cy.get("span.MuiTypography-root").should("contain", "1-10 of 20");
   });
 });
 
@@ -374,21 +248,12 @@ describe("Database Table Filtering Works", () => {
     cy.server();
     cy.route({
       url: `${SERVER_URL}/grades/`,
-      response: pageOne,
+      response: twoDegreeData,
       method: "GET",
       status: 200
     }).as("getModuleData");
-
-    cy.route({
-      url: `${SERVER_URL}/grades/?page=2`,
-      response: pageTwo,
-      method: "GET",
-      status: 200
-    }).as("getModuleDataPage2");
     cy.get(".getModuleDataButton").click();
-    cy.wait("@getModuleData");
-    cy.wait("@getModuleDataPage2");
-    cy.wait("@getModuleDataPage2");
+    cy.wait(["@getModuleData"]);
   });
 
   it("Has Correct Number of Discrete Options for Course Code and Grade", () => {
@@ -405,7 +270,7 @@ describe("Database Table Filtering Works", () => {
       .click();
     cy.get("ul.MuiList-root")
       .children()
-      .should("have.length", 15);
+      .should("have.length", 3);
   });
 
   it("Filters the Course Code", () => {
@@ -420,14 +285,14 @@ describe("Database Table Filtering Works", () => {
 
     cy.get(".MuiFormControl-root")
       .first()
-      .should("contain", "L45PROJ");
-    for (var index = 0; index < 10; index++) {
+      .should("contain", "INORG");
+    for (var index = 0; index < 3; index++) {
       cy.get(`tr[index=${index}]`)
         .children()
         .first()
-        .should("contain", "L45PROJ");
+        .should("contain", "INORG");
     }
-    cy.get("span.MuiTypography-root").should("contain", "1-10 of 20");
+    cy.get("span.MuiTypography-root").should("contain", "1-3 of 3");
   });
 
   it("Filters the Grade", () => {
@@ -443,13 +308,13 @@ describe("Database Table Filtering Works", () => {
     cy.get(".MuiFormControl-root")
       .last()
       .should("contain", "A1");
-    for (var index = 0; index < 3; index++) {
+    for (var index = 0; index < 2; index++) {
       cy.get(`tr[index=${index}]`)
         .children()
         .last()
         .should("contain", "A1");
     }
-    cy.get("span.MuiTypography-root").should("contain", "1-3 of 3");
+    cy.get("span.MuiTypography-root").should("contain", "1-2 of 2");
   });
 
   it("Filters the Matric Number", () => {
@@ -457,7 +322,7 @@ describe("Database Table Filtering Works", () => {
     cy.get(".MuiInputBase-inputTypeSearch").should("contain.value", "1000009");
     // Only to allow the table to rerender after the query filter
     cy.wait(50);
-    for (var index = 0; index < 2; index++) {
+    for (var index = 0; index < 1; index++) {
       cy.get(`tr[index=${index}]`)
         .children()
         .first()
