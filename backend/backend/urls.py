ce1f8520eb327c04ec0310eab005a557445e28ed
Mohammad Majid <2324127m@student.gla.ac.uk>
2019-12-04 12:44:35 +0000

    #105 [B] Separate API URLs from project URLs
    * Created new urls.py file in API
    * Migrated urls from project to API

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,15 +1,7 @@
 from django.urls import include, path
 from django.contrib import admin
-from rest_framework import routers
-
-from api import views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('grades/', views.GradeViewSet.as_view(), name='grades'),
-    path('students/', views.StudentViewSet.as_view(), name='students')
+    path('', include('api.urls')),
 ]
