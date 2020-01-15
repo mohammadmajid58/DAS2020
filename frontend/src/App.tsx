21ec7b644638ceb6dda622007ab86712220ad15d
AMeikle99 <aidenmeikle@ymail.com>
2020-01-15 14:41:46 +0000

    #113 [F] Create a New Navigation Bar Component
    I have created the Navigation Bar Component which allows the user to
    move between the three implemented pages
    
    * A new Navigation Bar component was created with the Uni Logo and 3
      Navigation Links, one for each of the 3 pages
    * The Index.css was updated to add styling for the Navbar Component
    * App.tsx was updated to remove the temporary Nav Links and these were
      moved to the NavBar
    * I added tests to ensure the Naviation links correctly go to each page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,18 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import UploadStudentRoster from "./pages/UploadStudentRoster";
 import UploadModuleMarks from "./pages/UploadModuleMarks";
 import ViewAllData from "./pages/ViewAllData";
 
+import NavigationBar from "./component/navigation/NavigationBar";
+
 class App extends Component {
   render() {
     return (
       <Router>
-        <Link to={"/upload-student-roster"} className="nav-link">
-          Upload Student Roster
-        </Link>
-
-        <Link to={"/upload-module-marks"} className="nav-link">
-          Upload Module Marks
-        </Link>
-
-        <Link to={"/view-all-data"} className="nav-link">
-          View All Data
-        </Link>
-
+        <NavigationBar />
         <Switch>
           <Route
             exact
