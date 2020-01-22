2e682e8294f6b7a311be0074c3fb2b95dc4e41f1
Mohammad Majid <2324127m@student.gla.ac.uk>
2020-01-22 14:02:02 +0000

    #134 [B] Population Script for an Academic Plan
    * Created Population Script for an Academic Plan
    * Works with oneDegreeData

--- backend/populate_database.py
+++ backend/populate_database.py
@@ -5,5 +5,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 django.setup()
 
 from population_scripts.admin_user import create_admin_user
+from population_scripts.academic_plan import create_academic_plan
 
 create_admin_user()
+create_academic_plan()
