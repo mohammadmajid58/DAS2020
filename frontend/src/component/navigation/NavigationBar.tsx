e39683e9d5c996f3a90b4f84cd9f56f75b4c0157
Raghad  abdulrab <raghadabdulrab@Raghads-MacBook-Pro.local>
2020-02-04 18:15:09 +0000

    #157 [F] Standard Formatting for Components
    * Using bootstrap, the navigation bar is made responsive
    * The dropzone has been centred in the page
    * The page titles have been style to be more prominent

--- frontend/src/component/navigation/NavigationBar.tsx
+++ frontend/src/component/navigation/NavigationBar.tsx
@@ -5,48 +5,69 @@ import { Button } from "@material-ui/core";
 export default class NavigationBar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-md navigationBar px-0">
+      <nav
+        className="navbar navbar-dark navbar-expand-lg"
+        style={{
+          backgroundColor: "#567aa8"
+        }}
+      >
         <div className="navLogo col-3 pr-0">
-          <a href="/"><img
-            src={require("./uog_logo.png")}
-            alt="University of Glasgow Logo"
-            width={250}
-            height={70}
-          /></a>
+          <a href="/">
+            <img
+              src={require("./uog_logo.png")}
+              alt="University of Glasgow Logo"
+              width={250}
+              height={70}
+            />
+          </a>
         </div>
-
-        <ul className="d-flex justify-content-between col-6">
-          <li className="nav-item rosterUploadLink">
-            <Button
-              to="/upload-student-roster"
-              component={Link}
-              className="navButton"
-            >
-              Upload Student Roster
-            </Button>
-          </li>
-          <li className="mx-10 nav-item marksUploadLink">
-            <Button
-              to="/upload-module-marks"
-              component={Link}
-              className="navButton"
-            >
-              Upload Module Marks
-            </Button>
-          </li>
-
-          <li className="nav-item allDataUploadLink">
-            <Button to="/view-all-data" component={Link} className="navButton">
-              View All Data
-            </Button>
-          </li>
-          <li className="nav-item adminSiteLink">
-            <button>
-              <a href="/admin/">Admin Tools</a>
-            </button>
-          </li>
-        </ul>
-        <div className="col-3"></div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item rosterUploadLink">
+              <Button
+                to="/upload-student-roster"
+                component={Link}
+                className="navButton"
+              >
+                Upload Student Roster
+              </Button>
+            </li>
+            <li className="mx-10 nav-item marksUploadLink">
+              <Button
+                to="/upload-module-marks"
+                component={Link}
+                className="navButton"
+              >
+                Upload Module Marks
+              </Button>
+            </li>
+            <li className="nav-item allDataUploadLink">
+              <Button
+                to="/view-all-data"
+                component={Link}
+                className="navButton"
+              >
+                View All Data
+              </Button>
+            </li>
+            <li className="nav-item adminSiteLink">
+              <button>
+                <a href="/admin/">Admin Tools</a>
+              </button>
+            </li>
+          </ul>
+        </div>
       </nav>
     );
   }
--- frontend/src/pages/UploadModuleMarks.tsx
+++ frontend/src/pages/UploadModuleMarks.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import StudentModuleMarkDropZone from "../component/upload_csv_files/StudentModuleMarkDropZone/StudentModuleMarkDropZone";
 import PageTitle from "../component/usability_components/PageTitle";
+import "../component/DesignElements/CentreContents.css";
 
 class UploadModuleMarks extends Component {
   render() {
@@ -8,7 +9,9 @@ class UploadModuleMarks extends Component {
       <div className="d-flex-inline">
         <PageTitle title="Upload Module Marks" />
         <div className="d-flex justify-content-center">
-          <StudentModuleMarkDropZone />
+          <div className="center">
+            <StudentModuleMarkDropZone />
+          </div>
         </div>
       </div>
     );
