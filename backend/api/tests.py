83aafa324e3a9d489797c57c8c3970d1fcf8590b
Kevin Lynch <kevin.e.lynch89@gmail.com>
2020-02-05 12:07:23 +0000

    #153 [B] Enforce academic plan weightings = 0.999 or 1
     Added a clean() method to the AcademicPlan model to ensure that the
     academic plan's course weightings sum to between 0.999 and 1. If they
     do not, the admin site form displays an informative error message to
     the user.
     Added a test case to check this is working correctly.
    
     Scrutinise:
     models.py
     tests.py

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -125,3 +125,43 @@ class FinalAwardTestCase(APITestCase):
         student = Student.objects.get(matricNo="2894029")
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(student.finalAward, 16)
+
+
+class AcademicPlanTestCase(APITestCase):
+
+    def _assert_plan_with_code_exists(self, code):
+        print("TEST_" + code + " " + str(AcademicPlan.objects.filter(planCode=code).exists()))
+        self.assertTrue(AcademicPlan.objects.filter(planCode=code).exists())
+
+    def _assert_plan_with_code_does_not_exist(self, code):
+        print("TEST_" + code + " " + str(AcademicPlan.objects.filter(planCode=code).exists()))
+        self.assertFalse(AcademicPlan.objects.filter(planCode=code).exists())
+
+    def test_weights_sum_correctly(self):
+        AcademicPlan.objects.create(planCode="TEST_a", courseCode="Chemist", mcName="abcde", mcCode="1234",
+                                    course_1="PSD", weight_1=0.9)
+        AcademicPlan.objects.create(planCode="TEST_b", courseCode="Chemist", mcName="abcde", mcCode="1234",
+                                    course_1="PSD", weight_1=0)
+        AcademicPlan.objects.create(planCode="TEST_c", courseCode="Chemist", mcName="abcde", mcCode="1234",
+                                    course_1="PSD", weight_1=1)
+        AcademicPlan.objects.create(planCode="TEST_d", courseCode="Chemist", mcName="abcde", mcCode="1234",
+                                    course_1="PSD", weight_1=0.5, course_2="c2", weight_2=0.5)
+        AcademicPlan.objects.create(planCode="TEST_e", courseCode="Chemist", mcName="abcde", mcCode="1234",
+                                    course_1="PSD", weight_1=0.25, course_2="c2", weight_2=0.25, course_3="c3",
+                                    weight_3=0.5)
+        AcademicPlan.objects.create(planCode="TEST_f", courseCode="Chemist", mcName="abcde", mcCode="1234",
+                                    course_1="PSD", weight_1=0.999)
+        AcademicPlan.objects.create(planCode="TEST_g", courseCode="Chemist", mcName="abcde", mcCode="1234",
+                                    course_1="PSD", weight_1=0.25, course_2="c2", weight_2=0.25, course_3="c3",
+                                    weight_3=0.5001)
+        AcademicPlan.objects.create(planCode="TEST_h", courseCode="Chemist", mcName="abcde", mcCode="1234",
+                                    course_1="PSD", weight_1=0.25, course_2="c2", weight_2=0.25, course_3="c3",
+                                    weight_3=0.4998)
+
+        codes_that_should_exist = ["c", "d", "e", "f", "h", "g"]
+        for code in codes_that_should_exist:
+            self._assert_plan_with_code_exists("TEST_" + code)
+
+        codes_that_should_not_exist = ["a", "b"]
+        for code in codes_that_should_not_exist:
+            self._assert_plan_with_code_does_not_exist("TEST_" + code)
