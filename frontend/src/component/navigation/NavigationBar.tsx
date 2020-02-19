d90033153019f3d80db04dc6ee2fa9298fff0ff4
Mohammad Majid <2324127m@student.gla.ac.uk>
2020-02-19 11:56:35 +0000

    Merge feature/auth into master - after rebase conflicts

--- frontend/src/component/navigation/NavigationBar.tsx
+++ frontend/src/component/navigation/NavigationBar.tsx
@@ -37,56 +37,58 @@ export default class NavigationBar extends Component<Props> {
           </a>
         </div>
         {this.props.isLoggedIn && (
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item rosterUploadLink">
-              <Button
-                to="/upload-student-roster"
-                component={Link}
-                className="navButton"
-              >
-                Upload Student Roster
-              </Button>
-            </li>
-            <li className="mx-10 nav-item marksUploadLink">
-              <Button
-                to="/upload-module-marks"
-                component={Link}
-                className="navButton"
-              >
-                Upload Module Marks
-              </Button>
-            </li>
-            <li className="nav-item allDataUploadLink">
-              <Button
-                to="/view-all-data"
-                component={Link}
-                className="navButton"
-              >
-                View All Data
-              </Button>
-            </li>
-            <li className="nav-item adminSiteLink">
-              <button>
-                <a href="/admin/">Admin Tools</a>
-              </button>
-            </li>
-            <li className="nav-item logout">
-              <button onClick={this.handleLogout}>Log Out</button>
-            </li>
-          </ul>
-        </div>
+          <div>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item rosterUploadLink">
+                  <Button
+                    to="/upload-student-roster"
+                    component={Link}
+                    className="navButton"
+                  >
+                    Upload Student Roster
+                  </Button>
+                </li>
+                <li className="mx-10 nav-item marksUploadLink">
+                  <Button
+                    to="/upload-module-marks"
+                    component={Link}
+                    className="navButton"
+                  >
+                    Upload Module Marks
+                  </Button>
+                </li>
+                <li className="nav-item allDataUploadLink">
+                  <Button
+                    to="/view-all-data"
+                    component={Link}
+                    className="navButton"
+                  >
+                    View All Data
+                  </Button>
+                </li>
+                <li className="nav-item adminSiteLink">
+                  <button>
+                    <a href="/admin/">Admin Tools</a>
+                  </button>
+                </li>
+                <li className="nav-item logout">
+                  <button onClick={this.handleLogout}>Log Out</button>
+                </li>
+              </ul>
+            </div>
+          </div>
         )}
       </nav>
     );
