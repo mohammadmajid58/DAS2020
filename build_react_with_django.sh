f3444f3576ddf5a9923353bb22a4c09072898e83
Mihnea Maldaianu <2289803m@student.gla.ac.uk>
2020-02-27 12:12:30 +0000

    Revert "Merge branch 'feature/refactor-build-script' into 'master'"
    This reverts merge request !123

--- build_react_with_django.sh
+++ build_react_with_django.sh
@@ -4,21 +4,14 @@
 rm -rf frontend/build
 rm -rf backend/react-frontend
 
-export running_in_dev=0
-if [[ "$1" = "DEV" ]]; then
-    export running_in_dev=1
-fi
-
-if [[ "$running_in_dev" -ne 1 ]]; then
-    # ensure debug setting is on false if not in DEV mode
-    if grep -e "DEBUG = True" backend/backend/settings.py; then
-        printf "\033[1;33mFound DEBUG = True in settings.py\033[0m\n"
-        printf "\033[1;33mIf you'd like to create a ZIP for Staging, please turn this to False now!\033[0m\n"
-        printf "\033[1;33mWould you like to continue? (y/n)\033[0m\n"
-        read result
-        if [[ ${result} == "n" ]]; then
-        exit 0;
-        fi
+# ensure debug setting is on false
+if grep -e "DEBUG = True" backend/backend/settings.py; then
+    printf "\033[1;33mFound DEBUG = True in settings.py\033[0m\n"
+    printf "\033[1;33mIf you'd like to create a ZIP for Staging, please turn this to False now!\033[0m\n"
+    printf "\033[1;33mWould you like to continue? (y/n)\033[0m\n"
+    read result
+    if [[ ${result} == "n" ]]; then
+	exit 0;
     fi
 fi
 
@@ -26,61 +19,37 @@ cd frontend
 
 if npm install; then
 
-    
-    if [[ "$running_in_dev" -eq 1 ]]; then
-        npm run-script build
-    else
-        printf "\033[1;33mBuild for Development or Staging? (type dev or staging)\033[0m\n"
-        read result
-        if [[ ${result} == "dev" ]]; then
-        npm run-script build
-        fi
-        if [[ ${result} == "staging" ]]; then
-        npm run-script build-staging
-        fi
-        if [[ ${result} != "dev" && ${result} != "staging" ]]; then
-        echo "Choose dev or staging, invalid input, try again..."
-        exit 0;
-        fi
-    fi
-    
     # We are currently in se01-main/frontend/
+    npm run build
     cd ..
     mkdir backend/react-frontend
     mv frontend/build backend/react-frontend/build
     cd backend
-    
-    if [[ "$running_in_dev" -eq 1 ]]; then
+    printf "\033[1;33mWould you like to run the Django Server with the latest Frontend? (y/n)\033[0m\n"
+    read result
+    if [[ ${result} == "y" ]]; then
         sh run.sh
-    else
-        printf "\033[1;33mWould you like to run the Django Server with the latest Frontend? (y/n)\033[0m\n"
-        read result
-        if [[ ${result} == "y" ]]; then
-            sh run.sh
-        fi
-    fi
+    fi    
 
-    if [[ "$running_in_dev" -ne 1 ]]; then
-        # We are currently in se01-main/backend/
-        printf "\033[1;33mWould you like to create a ZIP file for PythonAnywhere Staging? (y/n)\033[0m\n"
-        read result
-        if [[ ${result} == "y" ]]; then
-        if grep -e "DEBUG = True" backend/settings.py; then
-            printf "\033[1;33mYou cannot create a ZIP file for Staging with DEBUG = True, exiting...\033[0m\n"
-            exit 0;
+    # We are currently in se01-main/backend/
+    printf "\033[1;33mWould you like to create a ZIP file for PythonAnywhere Staging? (y/n)\033[0m\n"
+    read result
+    if [[ ${result} == "y" ]]; then
+	if grep -e "DEBUG = True" backend/settings.py; then
+	    printf "\033[1;33mYou cannot create a ZIP file for Staging with DEBUG = True, exiting...\033[0m\n"
+	    exit 0; 
+	fi
+	printf "\033[1;33mYou must run tests on both the frontend and backend!\033[0m\n"
+	printf "\033[1;33mHave you tested BOTH the frontend and backend? (y/n)\033[0m\n"
+	read result2
+	if [[ ${result2} == "n" ]]; then
+	    printf "\033[1;33mYou cannot create a Staging ZIP file without running tests..\033[0m\n"
+	    exit 0;
         fi
-        printf "\033[1;33mYou must run tests on both the frontend and backend!\033[0m\n"
-        printf "\033[1;33mHave you tested BOTH the frontend and backend? (y/n)\033[0m\n"
-        read result2
-        if [[ ${result2} == "n" ]]; then
-            printf "\033[1;33mYou cannot create a Staging ZIP file without running tests..\033[0m\n"
-            exit 0;
-            fi
-        
-            zip -r StagingFile.zip .
-        cd ..
-        mv backend/StagingFile.zip .
-        fi
+	
+        zip -r StagingFile.zip .
+	cd ..
+	mv backend/StagingFile.zip .
     fi
 
     # We are currently in se01-main/
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -9,16 +9,9 @@ import { getCookie } from "./abstract_functions";
 
 Axios.defaults.headers.common["X-CSRFToken"] = getCookie("csrftoken");
 
-const DEV_URL = "http://127.0.0.1:8000";
-const STAGING_URL = "http://teamdas123.pythonanywhere.com";
-
-const ENVIRONMENT = process.env.REACT_APP_STAGE;
-
-let API_URL: string;
-if (ENVIRONMENT === "dev") {
-  API_URL = DEV_URL;
-} else {
-  API_URL = STAGING_URL;
+let API_URL = "http://teamdas123.pythonanywhere.com";
+if (process.env.NODE_ENV === "development") {
+  API_URL = "http://127.0.0.1:8000";
 }
 
 export default API_URL;
