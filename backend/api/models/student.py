e6d825d28311af932d34a34e55af3474429d0376
Mohammad Majid <2324127m@student.gla.ac.uk>
2020-03-04 14:38:13 +0000

    #213 [B] Refactor student model for edge cases
    * Create tests to test edge cases for Student Model
    * Make any necessary changes to pass the tests

--- backend/api/models/student.py
+++ backend/api/models/student.py
@@ -1,14 +1,16 @@
 from decimal import Decimal
 
+from django.core.exceptions import ValidationError
+from django.core.validators import MinLengthValidator
 from django.db import models
 from api.models import AcademicPlan
 
 
 class Student(models.Model):
-    matricNo = models.CharField('Student', max_length=7, unique=True, primary_key=True)
-    givenNames = models.CharField('Given name(s)', max_length=40)
-    surname = models.CharField('Last name', max_length=25)
-    academicPlan = models.ForeignKey(AcademicPlan, on_delete=models.CASCADE, null=True)
+    matricNo = models.CharField('Student', max_length=7, validators=[MinLengthValidator(7)], primary_key=True)
+    givenNames = models.CharField('Given name(s)', max_length=64)
+    surname = models.CharField('Last name', max_length=32)
+    academicPlan = models.ForeignKey(AcademicPlan, on_delete=models.CASCADE, null=False)
     finalAward1 = models.DecimalField(max_digits=3, decimal_places=1, null=True)
     finalAward2 = models.DecimalField(max_digits=4, decimal_places=2, null=True)
     finalAward3 = models.DecimalField(max_digits=5, decimal_places=3, null=True, default=0.000)
@@ -24,7 +26,11 @@ class Student(models.Model):
             self.finalAward1 = Decimal(round(self.finalAward3, 1))
             self.finalAward2 = Decimal(round(self.finalAward3, 2))
             self.finalAward3 = Decimal(round(self.finalAward3, 3))
-        super().save(*args, **kwargs)
+        try:
+            int(self.matricNo)
+            super().save(*args, **kwargs)
+        except ValueError:
+            raise ValidationError("Student matric no must be a number.")
 
     def set_grade_data_updated(self):
         self.gradeDataUpdated = True
