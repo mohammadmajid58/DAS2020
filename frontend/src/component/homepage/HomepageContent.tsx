0b37ed377e93f0d04e1e968c5cdb39c568997ef6
Aiden Meikle <2302159m@student.gla.ac.uk>
2020-03-04 13:31:22 +0000

    #202 [F] Personalized Info on Home Page
    The Home Page had templating for Username and Date that needed to be
    filled in
    
    * The Date is retrieved and formatted into a Date Time Format
    * The Username is queried from the backend and displayed to the user to
    * On a formatting issue the Twitter component has been resized

--- frontend/src/component/homepage/HomepageContent.tsx
+++ frontend/src/component/homepage/HomepageContent.tsx
@@ -2,28 +2,50 @@ import React from "react";
 import "./HomepageContent.css";
 import { Timeline } from "react-twitter-widgets";
 
-const HomepageContent: React.FC = () => {
-  return (
-    <div className="d-flex justify-content-center">
-      <div className="left">
-        <p className="home-text">
-          [Date and time]<br/><br/>
-          Hello [User],<br/>
-          Welcome to DAS 2020.<br/><br/>
-          Use the navigation options above to upload or view student data.<br/>
-        </p>
-        <p className="home-text">
-          For guidance on how to use the system, view the documentation <u>here</u>.<br/>
-          To report a problem, contact your <u>system administrator</u>.
-        </p>
+interface Props {
+  date: String;
+  username: String;
+}
+
+class HomepageContent extends React.Component<Props> {
+  render() {
+    return (
+      <div className="d-flex justify-content-center">
+        <div className="left">
+          <div className="pt-5">
+            <p className="home-text">
+              {this.props.date}
+              <br />
+              <br />
+              Hello {this.props.username},
+              <br />
+              Welcome to DAS 2020.
+              <br />
+              <br />
+              Use the navigation options above to upload or view student data.
+              <br />
+            </p>
+            <p className="home-text">
+              For guidance on how to use the system, view the documentation{" "}
+              <u>here</u>.<br />
+              To report a problem, contact your <u>system administrator</u>.
+            </p>
+          </div>
+        </div>
+        <div className="right">
+          <Timeline
+            dataSource={{ sourceType: "profile", screenName: "UofGChem" }}
+            options={{
+              userName: "UofGChem",
+              height: "400",
+              width: "350",
+              margin: 0.25
+            }}
+          />
+        </div>
       </div>
-      <div className="right">
-        <Timeline dataSource={ { sourceType: "profile", screenName: "UofGChem" } }
-          options={ { userName: "UofGChem", height: "300", width: "250", margin: 0.25 } }
-          onLoad={() => console.log("Twitter Feed loaded!")} />
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default HomepageContent;
