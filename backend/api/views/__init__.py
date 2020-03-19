9d2deacd2015a01f8978b4493120189f76f68883
Mohammad Majid <2324127m@student.gla.ac.uk>
2020-03-19 21:29:03 +0000

    #230 [F] [B] Remove Temporary Reset Database
    * Remove temporary reset database component in Frontend
    * Remove API backend for reset database

--- backend/api/views/__init__.py
+++ backend/api/views/__init__.py
@@ -2,7 +2,6 @@ from .calculate import calculate
 from .index import index
 from .grade_view_set import GradeViewSet
 from .student_view_set import StudentViewSet
-from .temp_reset_database import temp_reset_database
 from .override_award import override_award
 from .get_student_grades import get_student_grades
 from .override_grade import override_grade
