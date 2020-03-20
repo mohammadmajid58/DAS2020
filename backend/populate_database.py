f20d5efd044ea4a917d863815513302e82e9cd5f
Kevin Lynch <kevin.e.lynch89@gmail.com>
2020-03-20 23:30:19 +0000

    #242 Remove unnecessary population scripts
     Removed the method calls to the academic plan and graduation year
     scripts in populate_database.py.

--- backend/populate_database.py
+++ backend/populate_database.py
@@ -5,11 +5,3 @@ import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 django.setup()
-
-from population_scripts.graduation_year import create_graduation_year
-from population_scripts.admin_user import create_admin_user
-from population_scripts.academic_plan import create_academic_plans
-
-create_graduation_year()
-create_admin_user()
-create_academic_plans()
